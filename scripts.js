fcbcbdfe9ca8aba3a15f09fb4bade5285f418118
Marat Enaliev <enamarat@gmail.com>
2021-02-05 13:02:26 +0300

    Create starting screen with a list of sections.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-//console.log("Hello!");
+// starting screen
+let openedSection = null;
+const goToSection = (event) => {
+  document.querySelector("#starting-screen").classList.add("hidden");
+  document.querySelector("header").classList.remove("hidden");
+
+  const regex = /#\w+/;
+  //console.log(event.target.href.match(regex));
+  openedSection = event.target.href.match(regex)[0];
+  document.querySelector(`${openedSection}`).classList.add("flex");
+}
+document.querySelector(".main-list").addEventListener("click", goToSection);
+
+const returnToMainSection = () => {
+  document.querySelector("#starting-screen").classList.remove("hidden");
+  document.querySelector(`${openedSection}`).className = "hidden";
+}
+document.querySelector("#main").addEventListener("click", returnToMainSection);
+
+// closed circuit
 let animationStarted = false;
 const startAnimation = () => {
   if (animationStarted === false) {
@@ -10,8 +29,6 @@ const startAnimation = () => {
     animationStarted = false;
     document.querySelector("#charge").className = "paused";
   }
-
-
 }
-//.addEventListener("animationend"
 document.querySelector("#start").addEventListener("click", startAnimation);
+//.addEventListener("animationend"
