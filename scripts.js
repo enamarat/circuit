89026c6542c859aa6435529ce650c6c85fcdb14f
Marat Enaliev <enamarat@gmail.com>
2021-03-10 20:39:18 +0300

    Add a sidebar.

--- scripts.js
+++ scripts.js
@@ -1,30 +1,39 @@
-// variable for the "circuit" section
-let switchTurnedOn = false;
-
 // starting screen
 let openedSection = null;
 const goToSection = (event) => {
   if (event.target.tagName === "A") {
-    document.querySelector("#starting-screen").classList.add("hidden");
+    document.querySelector(".starting-screen").style.width = "0";
+    document.querySelector("#lightning").style.display = "none";
+    document.querySelector(".starting-screen h1").style.display = "none";
+    document.querySelector(".starting-screen nav").style.display = "none";
     document.querySelector("header").classList.remove("hidden");
     const regex = /#\w+/;
+    // hide previous section
+    if (openedSection !== null) {
+        document.querySelector(`${openedSection}`).classList.remove("visible");
+        document.querySelector(`${openedSection}`).classList.add("hidden");
+    }
     openedSection = event.target.href.match(regex)[0];
     document.querySelector(`${openedSection}`).classList.add("visible");
   }
 }
 document.querySelector(".main-list").addEventListener("click", goToSection);
 
-const returnToMainSection = () => {
-  document.querySelector("#starting-screen").classList.remove("hidden");
-  document.querySelector(`${openedSection}`).className = "hidden";
-  // stop animation in "Circuit" section
-  document.querySelector("#switch").className = "neutral";
-  switchTurnedOn = false;
-  document.querySelector("#charge-circuit").className = "paused zIndexLow";
+const showSidebar = () => {
+  if (window.screen.width < 500) {
+      document.querySelector(".starting-screen").style.width = "90%";
+  } else {
+      document.querySelector(".starting-screen").style.width = "250px";
+  }
+
+  setTimeout(()=> {
+    document.querySelector(".starting-screen nav").style.display = "block";
+  },500);
 }
-document.querySelector("#main").addEventListener("click", returnToMainSection);
+document.querySelector("#hidden-nav").addEventListener("click", showSidebar);
 
 // circuit
+let switchTurnedOn = false;
 const toggleTheSwitch = () => {
   if (switchTurnedOn === false) {
     document.querySelector("#switch").className = "switch-on";
